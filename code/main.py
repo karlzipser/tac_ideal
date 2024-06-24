bed372933fac32a0c1cb918c65ed23548f054198
Karl Zipser <karlzipser@berkeley.edu>
2024-06-24 13:18:21 -0700

    gacp

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ from utilz2 import *
 if __name__ == '__main__':
     import sys
     print("Argument List:", str(sys.argv))
-    s=select_from_list['findideal','gen0']
+    s=select_from_list(['findideal','gen0'])
     if i=='findideal':
         from .findideal import *
     elif i=='gen0':
