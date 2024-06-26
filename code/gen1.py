6d04bf3fd6915c94705307a128e181f0de3d7bb0
Karl Zipser <karlzipser@berkeley.edu>
2024-06-25 22:28:55 -0700

    gacp

--- code/gen1.py
+++ code/gen1.py
@@ -14,7 +14,17 @@ if 'project_' in __file__:
 ##############################################################################
 from projutils import *
 from ..params.a import *
-
+import torch
+#import torch.nn as nn
+#import torch.nn.parallel
+#import torch.optim as optim
+#import torch.utils.data
+#import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import torchvision
+torchvision.disable_beta_transforms_warning()
+import torchvision.transforms.v2 as v2
 
 
 def get_transforms(d,image_size):
