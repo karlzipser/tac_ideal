a016fc25a0097687befb375054f608977a06f4f4
Karl Zipser <karlzipser@berkeley.edu>
2024-06-25 19:31:31 -0700

    gacp

--- params/a.py
+++ params/a.py
@@ -4,7 +4,7 @@ if host_name=='hiMac':
 	run_path='project_tac/20Jun24_12h29m11s'
 	run_path='project_tac/21Jun24_23h10m13s'
 elif host_name=='jane':
-	run_path='project_tac/25Jun24_18h48m47s'
+	run_path='project_tac/25Jun24_19h30m07s'
 device='cuda:1'
 batch_size=1
 num_workers=4
