674915ce57f32e40a71c2e91455ad37968a1653f
Karl Zipser <karlzipser@berkeley.edu>
2024-06-25 22:25:31 -0700

    gacp

--- code/gen1.py
+++ code/gen1.py
@@ -15,6 +15,72 @@ if 'project_' in __file__:
 from projutils import *
 from ..params.a import *
 
+
+transforms_dict=dict(
+    RandomPerspective=True,
+    RandomPerspective_distortion_scale=0.3,
+    RandomPerspective_p=0.3,
+    RandomPerspective_fill=_fill,
+
+    RandomRotation=True,
+    RandomRotation_angle=12,
+    RandomRotation_fill=_fill,
+
+    RandomResizedCrop=True,
+    RandomResizedCrop_scale=(0.85,1),
+    RandomResizedCrop_ratio=(0.85,1.2),
+
+    RandomHorizontalFlip=True,
+    RandomHorizontalFlip_p=0.5,
+        
+    RandomVerticalFlip=False,
+    RandomVerticalFlip_p=0.5,
+
+    RandomZoomOut=True,
+    RandomZoomOut_fill=_fill,
+    RandomZoomOut_side_range=(1.0,1.5),
+
+    ColorJitter=False,
+    ColorJitter_brightness=(0,1),
+    ColorJitter_contrast=(0,1),
+    ColorJitter_saturation=(0,2),
+    ColorJitter_hue=(-.03,.03),
+)
+transforms_dict2=dict(
+    RandomPerspective=True,
+    RandomPerspective_distortion_scale=0.5,
+    RandomPerspective_p=0.5,
+    RandomPerspective_fill=_fill,
+
+    RandomRotation=True,
+    RandomRotation_angle=16,
+    RandomRotation_fill=_fill,
+
+    RandomResizedCrop=True,
+    RandomResizedCrop_scale=(0.75,1),
+    RandomResizedCrop_ratio=(0.75,1.2),
+
+    RandomHorizontalFlip=True,
+    RandomHorizontalFlip_p=0.5,
+        
+    RandomVerticalFlip=False,
+    RandomVerticalFlip_p=0.5,
+
+    RandomZoomOut=True,
+    RandomZoomOut_fill=_fill,
+    RandomZoomOut_side_range=(1.0,1.5),
+
+    ColorJitter=False,
+    ColorJitter_brightness=(0,1),
+    ColorJitter_contrast=(0,1),
+    ColorJitter_saturation=(0,2),
+    ColorJitter_hue=(-.03,.03),
+)
+geometric_transforms_list,color_transforms_list=get_transforms(
+    d=transforms_dict,
+    image_size=(32,32))
+
+
 for repeat in range(repeats):
     cE(repeat)
     device = torch.device(device if torch.cuda.is_available() else 'cpu')
