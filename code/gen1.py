0d6419d7ffb536d7e0a42d482c79f9875b32c845
Karl Zipser <karlzipser@berkeley.edu>
2024-06-25 22:36:00 -0700

    gacp

--- code/gen1.py
+++ code/gen1.py
@@ -294,7 +294,7 @@ for repeat in range(repeats):
         [1,2,3,4,5],
     ]:
         target_neuron=0
-        ntimer=Timer(5*minutes)
+        ntimer=Timer(1*minutes)
         jitters=[-1,1]+100*[0]
         todo=list(range(10))
         while todo:
